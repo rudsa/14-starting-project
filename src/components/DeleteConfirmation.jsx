ca57bf41aea5cae9d205c4da82f201e5c5981bf3
rudsa <rudsa@naver.com>
2024-06-24 20:08:58 +0900

    useEffect

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,17 @@
+import React, { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    console.log("test1");
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+    return () => {
+      console.log("test2");
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
